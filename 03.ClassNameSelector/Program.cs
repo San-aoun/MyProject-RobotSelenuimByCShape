a5b64a9e5e8e52856e280677fc53fb909e3164eb
Piyathida Sanaoun (Numtarn) <39735390+San-aoun@users.noreply.github.com>
2019-02-17 17:02:29 +0700

    ๊Update

---
+++ 03.ClassNameSelector/Program.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+class EntryPoint
+{
+    static void Main()
+    {
+        string URL = "http://testing.todvachev.com/selectors/class-name/";
+        string ClassName = "testClass";
+
+        IWebDriver driver = new ChromeDriver();
+        driver.Navigate().GoToUrl(URL);
+        IWebElement element = driver.FindElement(By.ClassName(ClassName));
+
+        Console.WriteLine(element.Text);
+
+        driver.Close();
+    }
+}
---
+++ 04.CSSXpathSelector/Program.cs
@@ -0,0 +1,54 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+class EntryPoint
+{
+    static void Main()
+    {
+        string url = "http://testing.todvachev.com/selectors/css-path/";
+        string cssPath = "#post-108 > div > figure > img";
+        string xPath = "//*[@id=\"post-108\"]/div/figure/img";
+
+        IWebDriver driver = new ChromeDriver();
+        driver.Navigate().GoToUrl(url);
+        
+        IWebElement cssPathElement = driver.FindElement(By.CssSelector(cssPath));
+        IWebElement xPartElement = driver.FindElement(By.XPath(xPath));
+        
+
+        if (xPartElement.Displayed)
+        {
+            GreenMessage("Yes, I can see the element Xpath, It's rigth there!!");
+        }
+        else
+        {
+            RedMessage("No, Someting wrong!");
+        }
+
+        if (cssPathElement.Displayed)
+        {
+            GreenMessage("Yes, I can see the CSS, It's rigth there!!");
+        }
+        else
+        {
+            RedMessage("No, Someting wrong!");
+        }
+        driver.Close();
+    }
+
+    private static void RedMessage(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(message);
+        //Console.ForegroundColor = ConsoleColor.White;
+    }
+
+    private static void GreenMessage(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(message);
+        //Console.ForegroundColor = ConsoleColor.White;
+    }
+}
