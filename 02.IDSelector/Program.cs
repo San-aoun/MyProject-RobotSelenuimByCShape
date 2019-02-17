a1f659565fc3c4cb2b3a800ec62042168468a938
Piyathida Sanaoun (Numtarn) <39735390+San-aoun@users.noreply.github.com>
2019-02-17 14:12:19 +0700

    ID

---
+++ 02.IDSelector/Program.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+
+class EntryPoint
+{
+    static void Main()
+    {
+        String url = "http://testing.todvachev.com/selectors/id/";
+
+        IWebDriver driver = new ChromeDriver();
+        driver.Navigate().GoToUrl(url);
+        IWebDriver element = driver.FindElement(By.Id("comment"));
+    }
+}
+
