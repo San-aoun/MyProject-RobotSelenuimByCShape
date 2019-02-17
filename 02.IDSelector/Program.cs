bdf5b2ba5311e5df63d5f327076b29e4d65fc32e
Piyathida Sanaoun (Numtarn) <39735390+San-aoun@users.noreply.github.com>
2019-02-17 14:22:04 +0700

    Update Program.cs

--- 02.IDSelector/Program.cs
+++ 02.IDSelector/Program.cs
@@ -8,11 +8,39 @@ class EntryPoint
 {
     static void Main()
     {
-        String url = "http://testing.todvachev.com/selectors/id/";
+        string url = "http://testing.todvachev.com/selectors/id/";
+        string ID = "comment";
 
         IWebDriver driver = new ChromeDriver();
+
         driver.Navigate().GoToUrl(url);
-        IWebDriver element = driver.FindElement(By.Id("comment"));
+
+        IWebElement element = driver.FindElement(By.Id(ID));
+
+        if (element.Displayed)
+        {
+            GreenMessage("Yes, I can see the element, It's rigth there!!");
+        }
+        else
+        {
+            RedMessage("No, Someting wrong!");
+        }
+        driver.Close();
     }
+
+    private static void RedMessage(string Message)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(Message);
+
+    }
+
+    private static void GreenMessage(string Message)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(Message);
+    }
+
+    
 }
 
